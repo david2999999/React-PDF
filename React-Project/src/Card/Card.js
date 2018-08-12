b418bcc2c5d3374f95eab2f0fbd6ca4165f6328f
David Jiang <djiang86@binghamton.edu>
2018-08-12 01:52:35 +0000

    rendering elements with if statement

--- React-Project/src/Card/Card.js
+++ React-Project/src/Card/Card.js
@@ -2,14 +2,28 @@ import React, {Component} from 'react';
 import CheckList from "../CheckList/CheckList";
 
 class Card extends Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+            showDetails: false
+        };
+    }
+
     render() {
+        let cardDetails;
+        if (this.state.showDetails) {
+            cardDetails = (
+                <div className="card__details">
+                    {this.props.description}
+                    <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+                </div>
+            );
+        };
+
         return (
           <div className="card">
               <div className="card__title">{this.props.title}</div>
-              <div className="card__details">
-                  {this.props.description}
-                  <CheckList cardId={this.props.id} tasks={this.props.tasks} />
-              </div>
+              {cardDetails}
           </div>
         );
     }
