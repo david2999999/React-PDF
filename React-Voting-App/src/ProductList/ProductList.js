135bd48880f43bf1717d051237faec91cff4e651
David Jiang <djiang86@binghamton.edu>
2018-10-17 13:57:14 +0000

    react voting app

---
+++ React-Voting-App/src/ProductList/ProductList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+    render() {
+        return (
+            <div className='ui unstackable items'>
+                This is a basic react component
+            </div>
+        )
+    }
+}
+
+export default ProductList;
